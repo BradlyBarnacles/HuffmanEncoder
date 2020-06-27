f3b2d10396da7c36347ae86fb2d63493780c47b7
rats-can-fly <calum.octopus@gmail.com>
2020-06-27 14:49:11 +0100

    added input option for creation of huffman tree

--- compression.py
+++ compression.py
@@ -1,6 +1,13 @@
 import os
 import matplotlib.pyplot as plt
 import json
+import sys
+
+try:
+    rootdir = sys.argv[1]
+except:
+    rootdir = input("input text directoray\n")
+
 
 class Node:
 
@@ -39,17 +46,16 @@ char_frequency = {}
 
 
 
-for entry in os.listdir(path):
-    if "_sent_mail" in os.listdir(os.path.join(path, entry)):
-        directory = os.path.join(path, entry, "_sent_mail")
-        for file in os.listdir(directory):
-            with open(os.path.join(directory, file), 'r') as f:
-                for char in f.read():
-                    try:
-                        char_frequency[char] += 1
-                    except:
-                        char_frequency[char] = 1
-    break
+for root, subdirs, files in os.walk(rootdir):
+    for filename in files:
+        print(filename)
+        with open(os.path.join(root, filename), 'r') as f:
+            for char in f.read():
+                try:
+                    char_frequency[char] += 1
+                except:
+                    char_frequency[char] = 1
+
 
 nodes = []
 for char, freq in char_frequency.items():
@@ -65,6 +71,7 @@ while len(nodes) > 1:
     new.left = a
     new.right = b
     nodes.append(new)
+
 
 tree = nodes[0]
 
